db7a97a11fb71c59429a879fe051cfcd6b50776a
Tyler Stark <theirondeveloper@gmail.com>
2016-05-19 07:24:24 -0700

    Pulled styles out, added a load wheel
    Pulled out styles from <style> tag into a spreadsheet, and added
    a simple spreadsheet.

--- scripts/fibo.js
+++ scripts/fibo.js
@@ -6,11 +6,13 @@ var fiboInput = document.getElementById('fiboInput');
 var fiboOutput = document.getElementById('fiboOutput');
 
 fiboWorker.addEventListener('message', function(workerMessage) {
+  fiboForm.classList.remove('loading');
   fiboOutput.value = workerMessage.data;
 });
 
 fiboForm.addEventListener('submit', function(event) {
   event.preventDefault();
+  fiboForm.classList.add('loading');
   fiboOutput.setAttribute('placeholder', 'loading...');
   fiboOutput.value = '';
   fiboWorker.postMessage(fiboInput.value);
